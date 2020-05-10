3e437f3366f4469a8e8ec064d5adcc43afe11042
Florian <gerkensmeier.florian@gmx.de>
2020-05-11 00:57:51 +0200

    Read key File; Basic Structure for MessageCommands

--- src/discord/Discord.js
+++ src/discord/Discord.js
@@ -1,6 +1,7 @@
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const DiscordJS = require('discord.js');
+const fs = require('fs');;
 
 module.exports = class Discord {
     constructor(db){
@@ -10,7 +11,7 @@ module.exports = class Discord {
     }
 
     Start(){
-        this.client.login('NTg2OTE1NzY5NDkzODgwODQy.XpsVew.EFNs9EnYSZry687ZBS4RJhGToJA')
+        this.client.login(fs.readFileSync('discord.key', 'utf8'))
     }
 
     //Get all Events and then shoot them off to the next steop
@@ -53,6 +54,7 @@ module.exports = class Discord {
     //Based on the command we need to do different things
     FindCommand(type, eventData){
         let command = "";
+        let serverStorage = this.GetServerStorage(eventData);
         switch (type) {
             case "REACTIONADD":{
                 command += "Reactionadd";
@@ -66,7 +68,9 @@ module.exports = class Discord {
 
             case "MESSAGE":{
                 //Handle Messages
-                return false;
+                let content = eventData.content;
+                command += this.GetCommandFromMessageContent(content, serverStorage).then();
+                // umbauen Sodass das Promise direkt den Command Später auslöst
             }
 
             default:{
@@ -80,6 +84,50 @@ module.exports = class Discord {
 
     GetParamsFromMessage(message){
 
+    }
+
+    //Returns a Promise with the command or Error
+    GetCommandFromMessageContent(contentString, serverStorage){
+        let commandPromise = new Promise((resolve, reject) => {
+            let contenStringUP = contentString.toUpperCase();
+            let commands = serverStorage.get('commands').value();
+            for(let i in commands){
+                let name = commands[i].name.toUpperCase();
+                if(contenStringUP.includes(name)){
+                    resolve(commands[i]);
+                }
+            }
+
+            reject('COMMAND NOT FOUND:' + contentString);
+
+        });
+
+        return commandPromise;
+    }
+
+    //We need the Database for the Sprcific Server
+    //Create if not Found
+    GetServerStorage(event){
+        let guild = event.guild;
+        if(guild.available == false){
+            return null;
+        }
+
+        let guildId = guild.id;
+        let storagePath = 'storage/' + guildId + '/';
+        let storageFile = storagePath + 'config.json';
+
+        //If no Databse exists we need to create it.
+        if(fs.existsSync(storagePath) == false){
+            fs.mkdirSync(storagePath);
+            let database = low(new FileSync(storageFile));
+            //Create the BASE structure
+            database.defaults({commands: [], owner : guild.ownerID, forcedStart : '/'}).write();
+            database.get('commands').push({name : '/kick', filePath : 'kickUser'}).write();
+            return database;
+        }
+
+        return low(new FileSync(storageFile));
     }
 
     GetParamsFromReaction(reaction, type){
