c4d1c742423845e12effd1064e6362e9565ce70f
Florian Gerkensmeier <gerkensmeier.florian@gmx.de>
2020-06-22 11:44:20 +0200

    Error handling

--- src/Main.js
+++ src/Main.js
@@ -25,7 +25,7 @@ module.exports = class Main {
 
    AfterStart(){
       let db = low(new FileSync('storage/main.json'));
-      database.set('storagePath', 'storage/').write();     
+      db.set('storagePath', 'storage/').write();     
       return db;
    }
 
