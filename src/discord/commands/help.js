20ca5fb15c115205437813003881c91c6931c9c2
Florian <gerkensmeier.florian@gmx.de>
2020-08-30 13:39:03 +0200

    Databses not created at Bot join Fixes #4

---
+++ src/discord/commands/help.js
@@ -0,0 +1,107 @@
+const BasicCommand = require('./basicCommand.js').classObj;
+
+
+const defaults = {
+   command : "/help",
+   filePath : "help.js",
+   forcedStart : true,
+   enabled : true,
+   permissions : []
+};
+
+module.exports.defaults = defaults;
+module.exports.classObj = class addRoleForReaction extends BasicCommand{
+
+   constructor(discord, eventData, user, database, params){
+      super(discord, eventData, user, database, params); //call parent
+   }
+ 
+   execute(){
+      //no doing shit if we aren't even allowed to
+      if(this.isCommandAllowed(defaults.permissions) == false){
+         this.event.channel.send('You dont have the Permissions needed')
+         return false;
+      }   
+
+      //Get all commands
+      database.get('commands').value();
+      
+
+      let emoteArray = reactionHelper.getEmoteFromString(messageStr);
+      let roleMap = this.getMentionRoles();
+      
+      
+
+      //If we have more than one role only the last is used. Don't care right now to fix it TODO
+      let roleId = null;
+      roleMap.forEach(role => {
+         roleId = role.id; 
+      }); 
+
+      let emoteId = emoteArray[0]; //We know it has atleast one value
+
+      if(this.checkValid(emoteArray, roleMap) == false){
+         return;
+      }
+
+      let reactionDb = this.reactionDatabase();
+
+      this.updateDatabase(emoteId, reactionDb, roleId);
+      
+   }
+
+   checkValid(emoteArray, roleMap){
+      let returnVal = true;
+      if(emoteArray.length == 0){
+         this.event.channel.send('No Emote specified');
+         returnVal = false;
+      }
+
+      if(roleMap.size == 0){
+         this.event.channel.send('No Role specified');
+         returnVal = false;
+      }
+
+      return returnVal;
+   }
+
+   updateDatabase(emoteId, reactionDb, roleId){
+      let existing = reactionDb.get('roleAndEmote').find({emoteId : emoteId});
+
+      let resultObj = null; //init so no crash
+
+      let assignmentObj = {emoteId : emoteId, roleId: roleId};
+
+      //Update this one
+      if(existing.value() != null){ 
+         existing.update('roleId', () => roleId).write();
+         resultObj = existing.update('emoteId', () => emoteId).write();
+         this.respond(resultObj);
+         return true;
+      }
+
+      existing = reactionDb.get('roleAndEmote').find({roleId : roleId});
+
+      //Update this one instead
+      if(existing.value() != null){
+         existing.update('roleId', () => roleId).write();
+         resultObj = existing.update('emoteId', () => emoteId).write();
+         this.respond(resultObj);
+         return true;
+      }
+
+      //when not exists, make new
+      resultObj = reactionDb.get('roleAndEmote').push(assignmentObj).write();
+      this.respond(resultObj[0]); //this time we get an array
+      return true;
+   }
+
+   respond(resultObj){
+      let reactionHelper = this.getReactionHelper();
+      let emote = reactionHelper.getEmoteObjectFromId(resultObj.emoteId); //assume it exists for now. Should be checked at some point
+      let role = reactionHelper.getRoleById(resultObj.roleId); //assume it exists for now
+      this.event.channel.send('Role ' + role.name + ' is now tied to emote ' + emote.toString())
+   }
+
+
+}
