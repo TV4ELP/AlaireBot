96cb2d14c9f547d498761cb580f13944a85d37d3
Florian <gerkensmeier.florian@gmx.de>
2021-01-16 03:17:16 +0100

    comments

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const Main = require('./src/Main.js');
 
 
-
+//This is the entry point of everything. It only create a new Main Object
+//and that handles the actual startup
 let process = new Main();
 process.Start();
 
